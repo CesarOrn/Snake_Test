4ad531bb0b5ea61637be7ecd8fe93e2bb4034f78
Cesar Ornelas <Cesar Ornelas>
2016-05-27 14:17:30 -0600

    Signed-off-by: Fourshadow

--- Logic.cpp
+++ Logic.cpp
@@ -22,15 +22,12 @@ start =1;
 myrand = new MersenneTwister();
 
 
-
 view = new QGraphicsView(scene);
 view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
 
-
 body = new Body();
-
 
 
 view->setFixedSize(500,500);
@@ -70,11 +67,12 @@ void Logic::addBody()
 
     allBody[start]->setPos(allBody[start-1]->getLastX(),allBody[start-1]->getLastY());
     allBody[start]->setLastX(0);
-     allBody[start]->setLastY(0);
-     scene->addItem(allBody[start]);
+    allBody[start]->setLastY(0);
+    scene->addItem(allBody[start]);
     start++;
 }
 
+// updates the tail of the snake.
 void Logic::upDateBack()
 {
     int i =1;
@@ -83,9 +81,11 @@ void Logic::upDateBack()
         //qDebug()<<start;
         //qDebug()<<i;
 
-
+        // set the variable last position to the current postion.
         allBody[i]->setLastX(allBody[i]->x());
         allBody[i]->setLastY(allBody[i]->y());
+
+        // the postion of body at i to the i-1 last postion.
         allBody[i]->setPos(allBody[i-1]->getLastX(),allBody[i-1]->getLastY());
     i++;
     }
@@ -94,7 +94,7 @@ void Logic::upDateBack()
 
 void Logic::gameOver()
 {
-    // deletes objects be removing the scene
+    // deletes all snake and removed them from the scene.
     for(int i = 0; i<250000&& i<start;i++){
 
         scene->removeItem(allBody[i]);
@@ -108,59 +108,60 @@ void Logic::hitDetection()
 {
     QList <QGraphicsItem *> colliding_items = body->collidingItems();
     for(int i = 0 , n= colliding_items.size();i<n;i++){
-        if(typeid(*colliding_items[i])==typeid(Food)&&colliding_items[i]->x()!=allBody[0]->x()
-                &&colliding_items[i]->y()!=allBody[0]->y()){
-            qDebug()<< "hit food";
-           allBody[0]->scene()->removeItem(colliding_items[i]);
+
+        //find if you are hitting food and if you in the exact c
+        if(typeid(*colliding_items[i])==typeid(Food)
+                    &&colliding_items[i]->x()==allBody[0]->x()
+                    &&colliding_items[i]->y()==allBody[0]->y()){
+
+            // removes the Food item from the scene.
+            allBody[0]->scene()->removeItem(colliding_items[i]);
             spawnFood();
             addBody();
+            // deletes the body part from memory.
             delete(colliding_items[i]);
-        }else if (typeid(*colliding_items[i])==typeid(Body)&&colliding_items[i]->x()==allBody[0]->x()
+            //check if the head is touching a body and
+        }else if (typeid(*colliding_items[i])==typeid(Body)
+                  &&colliding_items[i]->x()==allBody[0]->x()
                   && colliding_items[i]->y()==allBody[0]->y()){
+            // stops timer that contorls the forward movement.
             timer->stop();
             gameOver();
             return;
         }
      }
+    //check of the head is off the screen then call game over if it is.
        if (allBody[0]->x()<0 ||allBody[0]->x()>490||
            allBody[0]->y()<0 || allBody[0]->y()>490){
 
         gameOver();
         }
 }
-
+// used to calculate which body part is in front and return for the x
 int Logic::calculateInFrontX(int first, int last)
 {
     return (allBody[first]->x()-allBody[last]->x())/5;
 }
 
+//caculates which body part is first on the y direction
 int Logic::calculateInFrontY(int first, int last)
 {
     return (allBody[first]->y()-allBody[last]->y())/5;
 }
 
-void Logic::msleep(unsigned long msecs)
-{
-    QThread::msleep(msecs);
-}
 
-unsigned long Logic::getRand()
-{
-    return myrand->genrand_int32();
-
-}
 
 void Logic::spawnFood()
 {
 
-    qDebug()<<"started";
+
     int x;
     int y;
 
     Food *food = new Food();
+    //get seed from a the time.
     srand(time(NULL));
     x=rand();
-    srand(time(NULL));
     y=rand();
     food->setRect(0,0,5,5);
     food->setPos((x*5)%490,(y*5)%490);
